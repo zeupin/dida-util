5168f41038ee5b0efa121632ee8cc0326cad9d9b
Macc Liu <maccliu@qq.com>
2017-11-14 21:42:38 +0800

    dev

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # dida-util
-Dida框架的一些工具类
+
+一些常用的工具类和工具函数。它是 [宙品科技](http://zeupin.com) 开源的 [Dida框架](http://dida.zeupin.com) 的一个功能组件。
+
+## 使用
+
+用Composer安装：
+
+```bash
+composer require dida/util
+```
+
+## 作者
+
+* [Macc Liu](https://github.com/maccliu)
+
+## 感谢
+
+* [宙品科技，Zeupin LLC](http://zeupin.com) , 尤其是 [Dida 框架团队](http://dida.zeupin.com)
+
+## 版权协议
+
+Copyright (c) 2017 Zeupin LLC. Released under the [MIT license](LICENSE)。
---
+++ src/Dida/Util/ArrayPlus.php
@@ -0,0 +1,84 @@
+<?php
+
+namespace Dida\Util;
+
+class ArrayPlus
+{
+    const VERSION = '20171114';
+
+
+    public static function assocBy(array &$array, $keyN)
+    {
+        if (!$array) {
+            return $array;
+        }
+
+        $args = func_get_args();
+        array_shift($args);
+        if (is_array($keyN)) {
+            $args = $keyN;
+        }
+
+        $return = [];
+
+        while ($row = array_shift($array)) {
+            $cur = &$return;
+
+            foreach ($args as $arg) {
+                if (!array_key_exists($arg, $row)) {
+                    return false;
+                }
+
+                $key = $row[$arg];
+
+                if (!array_key_exists($key, $cur)) {
+                    $cur[$key] = [];
+                }
+                $cur = &$cur[$key];
+            }
+
+
+            $cur = $row;
+        }
+
+        return $return;
+    }
+
+
+    public static function groupBy(array &$array, $keyN)
+    {
+        if (!$array) {
+            return $array;
+        }
+
+        $args = func_get_args();
+        array_shift($args);
+        if (is_array($keyN)) {
+            $args = $keyN;
+        }
+
+        $return = [];
+
+        while ($row = array_shift($array)) {
+            $cur = &$return;
+
+            foreach ($args as $arg) {
+                if (!array_key_exists($arg, $row)) {
+                    return false;
+                }
+
+                $key = $row[$arg];
+
+                if (!array_key_exists($key, $cur)) {
+                    $cur[$key] = [];
+                }
+                $cur = &$cur[$key];
+            }
+
+
+            $cur[] = $row;
+        }
+
+        return $return;
+    }
+}
